ea7c42c3b8047a9281c6171eaf439cb3020b8953
kuroppe <kuroppems@gmail.com>
2020-05-02 09:20:35 +0900

    production build の設定

---
+++ webpack/webpack.common.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const srcDir = '../src';
+const BabelMinifyPlugin = require('babel-minify-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, `${srcDir}/Index.tsx`),
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        host: '0.0.0.0',
+        port: 8080,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: ['ts-loader', 'babel-loader'],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    plugins: [
+        new BabelMinifyPlugin(),
+        new HtmlWebpackPlugin({
+            publicPath: 'dist',
+            filename: 'index.html',
+            template: `static/index.html`,
+        }),
+    ],
+};
