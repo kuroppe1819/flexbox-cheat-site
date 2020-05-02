e5a92ccc35af286827f00a3a7b3a90c9a0d69916
kuroppe <kuroppems@gmail.com>
2020-05-03 01:23:49 +0900

    Google Fontの導入

--- src/Index.tsx
+++ src/Index.tsx
@@ -6,6 +6,7 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import 'normalize.css';
+import '../static/css/style.css';
 
 library.add(fab, fas, far);
 ReactDOM.render(<IndexMain />, document.getElementById('root'));
