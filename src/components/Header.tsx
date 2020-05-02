fa59518f99c421d45a66b27027d3dd8ef5786ea4
kuroppe <kuroppems@gmail.com>
2020-05-03 01:19:41 +0900

    Headerの作成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import tw from 'tailwind.macro';
 
 export const Header = () => {
     return (
-        <header className="header">
-            <h1 className="header__title">FlexBox Cheat Site</h1>
-            <FontAwesomeIcon icon={['fas', 'share-alt']} />
-            <FontAwesomeIcon icon={['fab', 'github']} />
-        </header>
+        <HeaderMain>
+            <HeaderTitle>FlexBox Cheat Site</HeaderTitle>
+            <LinkIcons>
+                <ExternalLink>
+                    <FontAwesomeIcon icon={['fas', 'share-alt']} size="2x" />
+                </ExternalLink>
+                <ExternalLink>
+                    <FontAwesomeIcon icon={['fab', 'github']} size="2x" />
+                </ExternalLink>
+            </LinkIcons>
+        </HeaderMain>
     );
 };
+
+const HeaderMain = styled.header`
+    ${tw`text-sm flex h-16 justify-center items-center bg-white shadow-md`}
+`;
+
+const HeaderTitle = styled.h1`
+    ${tw`m-0 text-blue-900`};
+`;
+
+const LinkIcons = styled.div`
+    ${tw`absolute right-0 text-blue-900`};
+`;
+
+const ExternalLink = styled.a`
+    ${tw`inline-block h-8 mr-5`};
+`;
