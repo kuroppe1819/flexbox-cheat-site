5f41571458d3acb8b71279b208fa9f527d6a8c94
kuroppe <kuroppems@gmail.com>
2020-05-03 01:23:30 +0900

    リセットCSSの導入

--- src/Index.tsx
+++ src/Index.tsx
@@ -5,6 +5,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
+import 'normalize.css';
 
 library.add(fab, fas, far);
 ReactDOM.render(<IndexMain />, document.getElementById('root'));
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -24,6 +24,10 @@ module.exports = {
                 use: ['babel-loader'],
                 exclude: /node_modules/,
             },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
         ],
     },
     resolve: {
