c04d1a155064295ad8ae8f8111cfd8254f68dbed
kuroppe <kuroppems@gmail.com>
2020-05-04 13:49:45 +0900

    BehaiviorFlexboxを追加

---
+++ stories/FlexItem.stories.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+import { FlexItem } from '../src/flex/FlexItem';
+
+export default {
+    title: 'FlexItem',
+    component: FlexItem,
+};
+
+export const Base = (): ReactElement => <FlexItem />;
