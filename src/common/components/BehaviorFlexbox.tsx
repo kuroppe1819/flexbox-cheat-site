16f952eabd7fbfb7a41ec6e3df212d77454a18cb
kuroppe <kuroppems@gmail.com>
2020-05-11 23:42:05 +0900

    各項目の間隔を微調整

--- src/common/components/BehaviorFlexbox.tsx
+++ src/common/components/BehaviorFlexbox.tsx
@@ -28,7 +28,7 @@ export const BehaviorFlexbox = (props: Props): ReactElement => {
 };
 
 const BehaviorBoxWrapper = styled.div`
-    ${tw`m-3`}
+    ${tw`mx-3 mt-3 mb-5`}
 `;
 
 const BehaviorBox = styled.div`
@@ -41,5 +41,5 @@ const ExampleItems = styled.div<{ parentStyle: FlattenSimpleInterpolation }>`
 `;
 
 const PropertyValue = styled.p`
-    ${tw`text-lg text-center text-gray-500 mt-3 mb-3`}
+    ${tw`text-lg text-center text-gray-500 mt-2 mb-3`}
 `;
