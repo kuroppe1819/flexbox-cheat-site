98518d66c6b921cc7ef53ca2d101ef370efbf37b
kuroppe <kuroppems@gmail.com>
2020-05-11 20:12:52 +0900

    justify-contentを追加

---
+++ src/flexboxItems/justifyContent/JCSpaceEvenlyContainer.tsx
@@ -0,0 +1,24 @@
+import React, { Dispatch, ReactElement, SetStateAction } from 'react';
+import { createCssCodeForParent } from '../../common/util/CreateCssCode';
+import { BehaviorFlexbox } from '../../common/components/BehaviorFlexbox';
+import { css } from 'styled-components';
+
+type Props = {
+    setParentStyle: Dispatch<SetStateAction<string>>;
+};
+
+const PROPERTY_NAME = 'space-evenly';
+
+const PARENT_STYLE_CONTENTS = `display: flex;
+    justify-content: space-evenly;`;
+
+export const JCSpaceEvenlyContainer = (props: Props): ReactElement => {
+    const parentStyleCssCode = createCssCodeForParent(PARENT_STYLE_CONTENTS);
+    const onClickBoxHandler = (): void => props.setParentStyle(parentStyleCssCode);
+    const parentStyle = css`
+        ${PARENT_STYLE_CONTENTS}
+    `;
+    return (
+        <BehaviorFlexbox parentStyle={parentStyle} propertyName={PROPERTY_NAME} onClickBoxHandler={onClickBoxHandler} />
+    );
+};
