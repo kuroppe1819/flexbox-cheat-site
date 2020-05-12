1fb87da6d987c27531318b163ad255e95472235b
kuroppe <kuroppems@gmail.com>
2020-05-12 12:26:25 +0900

    align-contentを追加

---
+++ src/flexboxItems/alignContent/ACFlexStartContainer.tsx
@@ -0,0 +1,36 @@
+import React, { Dispatch, ReactElement, SetStateAction } from 'react';
+import { createCssCodeForParent } from '../../common/util/CreateCssCode';
+import { BehaviorFlexbox } from '../../common/components/BehaviorFlexbox';
+import { css } from 'styled-components';
+
+type Props = {
+    setParentStyle: Dispatch<SetStateAction<string>>;
+};
+
+const PROPERTY_NAME = 'flex-start';
+
+const PARENT_STYLE_CONTENTS = `display: flex;
+    flex-wrap: wrap;
+    align-content: flex-start;
+    height: 100%;`;
+
+const CHILD_STYLE_CONTENTS = `width: 30%;`;
+
+export const ACFlexStartContainer = (props: Props): ReactElement => {
+    const parentStyleCssCode = createCssCodeForParent(PARENT_STYLE_CONTENTS);
+    const onClickBoxHandler = (): void => props.setParentStyle(parentStyleCssCode);
+    const parentStyle = css`
+        ${PARENT_STYLE_CONTENTS}
+    `;
+    const childStyle = css`
+        ${CHILD_STYLE_CONTENTS}
+    `;
+    return (
+        <BehaviorFlexbox
+            parentStyle={parentStyle}
+            childStyle={childStyle}
+            propertyName={PROPERTY_NAME}
+            onClickBoxHandler={onClickBoxHandler}
+        />
+    );
+};
