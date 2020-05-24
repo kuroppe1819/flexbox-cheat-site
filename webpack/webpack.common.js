2b512e1aacf46efc78f0870238691c18fbf9e814
kuroppe <kuroppems@gmail.com>
2020-05-24 17:10:29 +0900

    webpackのチャンク設定

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -8,6 +8,12 @@ module.exports = {
         path: path.resolve(__dirname, '../dist'),
         filename: 'bundle.js',
     },
+    optimization: {
+        splitChunks: {
+            name: 'vendor',
+            chunks: 'initial',
+        },
+    },
     devServer: {
         contentBase: path.resolve(__dirname, 'dist'),
         host: '0.0.0.0',
