bb659e9a3b0184a3367917bfc7c57bc6babcc5d5
kuroppe <kuroppems@gmail.com>
2020-09-19 19:01:39 +0900

    :recycle: Operation check for StyledComopnent
    🎉 - :tada: - Begin a project.
    ✨ - :sparkles: - Introduce new features.
    🔥 - :fire: - Remove code or files.
    🐛 - :bug: - Fix a bug.
    ✏️  - :pencil2: - Fix typos.
    ⚡️ - :zap: - Improve performance.
    📝 - :memo: - Add or update documentation.
    🚀 - :rocket: - Deploy stuff.
    💄 - :lipstick: - Add or update the UI and style files.
    ✅ - :white_check_mark: - Add or update tests.
    🔒 - :lock: - Fix security issues.
    🔖 - :bookmark: - Release / Version tags.
    🚧 - :construction: - Work in progress.
    💚 - :green_heart: - Fix CI Build.
    ♻️  - :recycle: - Refactor code.
    🔧 - :wrench: - Add or update configuration files.
    💩 - :poop: - Write bad code that needs to be improved.

---
+++ src/NumberBlock.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type Props = {
+    text: string;
+};
+
+const Component: React.FC<Props & StyledProps> = (props: Props & StyledProps) => {
+    return <div className={props.className}>Hello World</div>;
+};
+
+const StyledComponent = styled(Component)`
+    color: red;
+    background-color: blue;
+`;
+
+export const NumberBlock = StyledComponent;
