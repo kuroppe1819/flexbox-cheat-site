767aa002d9824cccc8c6bdce297c9edc2ea2b605
kuroppe <kuroppems@gmail.com>
2020-09-19 15:20:44 +0900

    ♻️ Show temporarily the index2.tsx because we will remove depend libraries

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, `../src/Index.tsx`),
+    entry: path.resolve(__dirname, `../src/index2.tsx`),
     output: {
         path: path.resolve(__dirname, '../dist'),
         filename: 'bundle.js',
