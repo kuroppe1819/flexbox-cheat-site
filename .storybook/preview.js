4ee19921d66d6384b7078e14ccf5580b6d6e160d
kuroppe <kuroppems@gmail.com>
2020-09-20 12:27:42 +0900

    :lipstick: Import Fontawesome icons
    🎉 - :tada: - Begin a project.
    ✨ - :sparkles: - Introduce new features.
    🔥 - :fire: - Remove code or files.
    🐛 - :bug: - Fix a bug.
    ✏️  - :pencil2: - Fix typos.
    ⚡️ - :zap: - Improve performance.
    📝 - :memo: - Add or update documentation.
    🚀 - :rocket: - Deploy stuff.
    💄 - :lipstick: - Add or update the UI and style files.
    ✅ - :white_check_mark: - Add or update tests.
    🔒 - :lock: - Fix security issues.
    🔖 - :bookmark: - Release / Version tags.
    🚧 - :construction: - Work in progress.
    💚 - :green_heart: - Fix CI Build.
    ♻️  - :recycle: - Refactor code.
    🔧 - :wrench: - Add or update configuration files.
    💩 - :poop: - Write bad code that needs to be improved.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,19 +3,24 @@ import { ThemeProvider } from 'styled-components';
 import { Normalize } from 'styled-normalize';
 import { GlobalStyle } from '../src/components/GlobalStyle';
 import { theme } from '../src/fixtures/theme';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
 };
 
 export const decorators = [
-    (Story) => (
-        <>
-            <Normalize />
-            <GlobalStyle />
-            <ThemeProvider theme={theme}>
-                <Story />
-            </ThemeProvider>
-        </>
-    ),
+    (Story) => {
+        library.add(faTwitter, faGithub);
+        return (
+            <>
+                <Normalize />
+                <GlobalStyle />
+                <ThemeProvider theme={theme}>
+                    <Story />
+                </ThemeProvider>
+            </>
+        );
+    },
 ];
--- src/Index.tsx
+++ src/Index.tsx
@@ -5,12 +5,18 @@ import { Normalize } from 'styled-normalize';
 import { GlobalStyle } from './components/GlobalStyle';
 import { NumberBlock } from './components/NumberBlock';
 import { theme } from './fixtures/theme';
+import { Header } from './components/Header';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faTwitter, faGithub);
 
 ReactDOM.render(
     <React.StrictMode>
         <Normalize />
         <GlobalStyle />
         <ThemeProvider theme={theme}>
+            <Header />
             <NumberBlock index={1} />
         </ThemeProvider>
     </React.StrictMode>,
