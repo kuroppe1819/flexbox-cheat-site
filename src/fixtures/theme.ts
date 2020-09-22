db9b43d604d51e5e24c82baaa5bf0e8331d662e7
kuroppe <kuroppems@gmail.com>
2020-09-22 17:20:10 +0900

    :sparkles: Create json data of Flexbox properties
    🎉 - :tada: - Begin a project.
    ✨ - :sparkles: - Introduce new features.
    🔥 - :fire: - Remove code or files.
    🐛 - :bug: - Fix a bug.
    ✏️  - :pencil2: - Fix typos.
    ⚡️ - :zap: - Improve performance.
    📝 - :memo: - Add or update documentation.
    🚀 - :rocket: - Deploy stuff.
    💄 - :lipstick: - Add or update the UI and style files.
    ✅ - :white_check_mark: - Add or update tests.
    🔒 - :lock: - Fix security issues.
    🔖 - :bookmark: - Release / Version tags.
    🚧 - :construction: - Work in progress.
    💚 - :green_heart: - Fix CI Build.
    ♻️  - :recycle: - Refactor code.
    🔧 - :wrench: - Add or update configuration files.
    💩 - :poop: - Write bad code that needs to be improved.

--- src/fixtures/theme.ts
+++ src/fixtures/theme.ts
@@ -4,6 +4,7 @@ export const theme = {
         blue100: '#ebf8ff',
         blue400: '#63b3ed',
         blue500: '#4299e1',
+        blue700: '#2b6cb0',
         blue900: '#2a4365',
         gray400: '#cbd5e0',
         gray500: '#a0aec0',
