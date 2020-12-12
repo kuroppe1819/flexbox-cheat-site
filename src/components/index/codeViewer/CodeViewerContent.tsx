3a1dac88fa6e93d665bd924ade9600cf0cbd0513
kuroppe <kuroppems@gmail.com>
2020-12-12 15:07:02 +0900

    :sparkles: Create CodeViewerContent component.
    🎉 - :tada: - Begin a project.
    ✨ - :sparkles: - Introduce new features.
    🔥 - :fire: - Remove code or files.
    🐛 - :bug: - Fix a bug.
    ✏️  - :pencil2: - Fix typos.
    ⚡️ - :zap: - Improve performance.
    📝 - :memo: - Add or update documentation.
    🚀 - :rocket: - Deploy stuff.
    💄 - :lipstick: - Add or update the UI and style files.
    ✅ - :white_check_mark: - Add or update tests.
    🔒 - :lock: - Fix security issues.
    🔖 - :bookmark: - Release / Version tags.
    🚧 - :construction: - Work in progress.
    💚 - :green_heart: - Fix CI Build.
    ♻️  - :recycle: - Refactor code.
    🔧 - :wrench: - Add or update configuration files.
    💩 - :poop: - Write bad code that needs to be improved.

---
+++ src/components/index/codeViewer/CodeViewerContent.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+import monoBlue from 'react-syntax-highlighter/dist/esm/styles/hljs/mono-blue';
+import styled from 'styled-components';
+import { deviceMaxWidth } from '../../../data/deviceSize';
+import { FileExtension } from './CodeViewer';
+
+type Props = {
+    fileExtension: FileExtension;
+    sourceCode: string;
+};
+
+const Component: React.FC<Props & StyledProps> = ({ className, fileExtension, sourceCode }) => (
+    <div className={className}>
+        <SyntaxHighlighter className={`${className}__syntaxHighlighter`} language={fileExtension} style={monoBlue}>
+            {sourceCode}
+        </SyntaxHighlighter>
+    </div>
+);
+
+const StyledComponent: React.FC<Props> = styled(Component)`
+    font-size: 1.1rem;
+
+    &__syntaxHighlighter {
+        margin: 0.75rem 0.5rem;
+        background-color: ${({ theme }) => theme.color.blue400};
+        min-height: 3rem;
+        height: 15rem;
+
+        @media ${deviceMaxWidth.laptop} {
+            height: 10rem;
+        }
+
+        @media ${deviceMaxWidth.mobileL} {
+            height: 8rem;
+        }
+    }
+`;
+
+export const SourceCodeViewer = StyledComponent;
