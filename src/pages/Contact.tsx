dc74216a6198e2cc8da5c8b9d54d97f9fbc01083
kuroppe <kuroppems@gmail.com>
2020-12-20 23:21:54 +0900

    :sparkles: Define a message of page title.
    🎉 - :tada: - Begin a project.
    ✨ - :sparkles: - Introduce new features.
    🔥 - :fire: - Remove code or files.
    🐛 - :bug: - Fix a bug.
    ✏️  - :pencil2: - Fix typos.
    ⚡️ - :zap: - Improve performance.
    📝 - :memo: - Add or update documentation.
    🚀 - :rocket: - Deploy stuff.
    💄 - :lipstick: - Add or update the UI and style files.
    ✅ - :white_check_mark: - Add or update tests.
    🔒 - :lock: - Fix security issues.
    🔖 - :bookmark: - Release / Version tags.
    🚧 - :construction: - Work in progress.
    💚 - :green_heart: - Fix CI Build.
    ♻️  - :recycle: - Refactor code.
    🔧 - :wrench: - Add or update configuration files.
    💩 - :poop: - Write bad code that needs to be improved.

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,26 +3,31 @@ import styled from 'styled-components';
 import { BackToMainPageLink } from '../components/pages/BackToMainPageLink';
 import { CustomIntlProvider, Language } from '../components/providers/CustomIntlProvider';
 import { PageRoot } from '../components/root/PageRoot';
+import { useCustomIntl } from '../fixtures/hooks/useCustomIntl';
 
-const Component: React.VFC<StyledProps> = ({ className }) => (
-    <PageRoot>
-        <main className={`${className}`}>
-            <h2>お問い合わせ</h2>
-            <p>
-                お問い合わせは{' '}
-                <a href="https://twitter.com/mys_x101" target="_blank" rel="noopener noreferrer">
-                    Twitter
-                </a>{' '}
-                の DM にて承っております。
-            </p>
-            <p>サービスの改善についてのご意見、ご要望をお聞かせください！</p>
-            <p>応援コメントもお待ちしております！</p>
-            <div className={`${className}__backToMainPageLink`}>
-                <BackToMainPageLink />
-            </div>
-        </main>
-    </PageRoot>
-);
+const Component: React.VFC<StyledProps> = ({ className }) => {
+    const { formatMessage } = useCustomIntl();
+
+    return (
+        <PageRoot>
+            <main className={`${className}`}>
+                <h2>{formatMessage({ id: 'contact.title' })}</h2>
+                <p>
+                    お問い合わせは{' '}
+                    <a href="https://twitter.com/mys_x101" target="_blank" rel="noopener noreferrer">
+                        Twitter
+                    </a>{' '}
+                    の DM にて承っております。
+                </p>
+                <p>サービスの改善についてのご意見、ご要望をお聞かせください！</p>
+                <p>応援コメントもお待ちしております！</p>
+                <div className={`${className}__backToMainPageLink`}>
+                    <BackToMainPageLink />
+                </div>
+            </main>
+        </PageRoot>
+    );
+};
 
 const StyledComponent: React.VFC = styled(Component)`
     max-width: 37.5rem;
