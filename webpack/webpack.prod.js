6df35b2f75871eb50c3fb10cea4a0b6d7723e6f7
kuroppe <kuroppems@gmail.com>
2021-01-03 16:08:29 +0900

    :sparkles: Add a sitemap.
    🎉 - :tada: - Begin a project.
    ✨ - :sparkles: - Introduce new features.
    🔥 - :fire: - Remove code or files.
    🐛 - :bug: - Fix a bug.
    ✏️  - :pencil2: - Fix typos.
    ⚡️ - :zap: - Improve performance.
    📝 - :memo: - Add or update documentation.
    🚀 - :rocket: - Deploy stuff.
    💄 - :lipstick: - Add or update the UI and style files.
    ✅ - :white_check_mark: - Add or update tests.
    🔒 - :lock: - Fix security issues.
    🔖 - :bookmark: - Release / Version tags.
    🚧 - :construction: - Work in progress.
    💚 - :green_heart: - Fix CI Build.
    ♻️  - :recycle: - Refactor code.
    🔧 - :wrench: - Add or update configuration files.
    💩 - :poop: - Write bad code that needs to be improved.

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -4,9 +4,9 @@ const common = require('./webpack.common.js');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const CopyPlugin = require('copy-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest')
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
-const cacheId = "flex";
+const cacheId = 'flex';
 
 module.exports = (env, argv) => {
     const isAnalyze = argv.analyze === undefined;
@@ -14,9 +14,10 @@ module.exports = (env, argv) => {
         new CopyPlugin({
             patterns: [
                 { from: 'static/image', to: 'image' },
-                { from: "static/robots.txt", to: "" },
-                { from: "static/CNAME", to: "" }
-            ]
+                { from: 'static/robots.txt', to: '' },
+                { from: 'static/CNAME', to: '' },
+                { from: 'static/sitemap.xml', to: '' },
+            ],
         }),
         new WorkboxPlugin.GenerateSW({
             cacheId: cacheId,
@@ -28,9 +29,9 @@ module.exports = (env, argv) => {
             runtimeCaching: [
                 {
                     urlPattern: /.+(\/|.html)$/,
-                    handler: "NetworkFirst",
+                    handler: 'NetworkFirst',
                     options: {
-                        cacheName: cacheId + "-html",
+                        cacheName: cacheId + '-html',
                         expiration: {
                             maxAgeSeconds: 60 * 60 * 24 * 7,
                         },
@@ -38,68 +39,68 @@ module.exports = (env, argv) => {
                 },
                 {
                     urlPattern: /.+\.(js|css|png|jpg|jpeg|svg)$/,
-                    handler: "CacheFirst",
+                    handler: 'CacheFirst',
                     options: {
-                        cacheName: cacheId + "-dependent",
+                        cacheName: cacheId + '-dependent',
                         expiration: {
                             maxAgeSeconds: 60 * 60 * 24 * 30,
                         },
                     },
                 },
-            ]
+            ],
         }),
         new WebpackPwaManifest({
-            filename: "manifest.webmanifest",
-            short_name: "Flexbox Cheat Site",
-            name: "Flexbox Cheat Site",
-            description: "A simple web site for flexbox cheatsheet.",
-            start_url: "/en",
-            display: "standalone",
-            theme_color: "#2a4365",
-            background_color: "#ffffff",
+            filename: 'manifest.webmanifest',
+            short_name: 'Flexbox Cheat Site',
+            name: 'Flexbox Cheat Site',
+            description: 'A simple web site for flexbox cheatsheet.',
+            start_url: '/en',
+            display: 'standalone',
+            theme_color: '#2a4365',
+            background_color: '#ffffff',
             icons: [
-              {
-                src: 'static/image/logo16.png',
-                size: "16x16",
-                type: "image/png"
-              },
-              {
-                src: 'static/image/logo24.png',
-                size: "24x24",
-                type: "image/png"
-              },
-              {
-                src: 'static/image/logo32.png',
-                size: "32x32",
-                type: "image/png"
-              },
-              {
-                src: 'static/image/logo48.png',
-                size: "48x48",
-                type: "image/png"
-              },
-              {
-                src: 'static/image/logo96.png',
-                size: "96x96",
-                type: "image/png"
-              },
-              {
-                src: 'static/image/logo144.png',
-                size: "144x144",
-                type: "image/png"
-              },
-              {
-                src: 'static/image/logo192.png',
-                size: "192x192",
-                type: "image/png"
-              },
-              {
-                src: 'static/image/logo512.png',
-                size: "512x512",
-                type: "image/png"
-              }
-            ]
-          })
+                {
+                    src: 'static/image/logo16.png',
+                    size: '16x16',
+                    type: 'image/png',
+                },
+                {
+                    src: 'static/image/logo24.png',
+                    size: '24x24',
+                    type: 'image/png',
+                },
+                {
+                    src: 'static/image/logo32.png',
+                    size: '32x32',
+                    type: 'image/png',
+                },
+                {
+                    src: 'static/image/logo48.png',
+                    size: '48x48',
+                    type: 'image/png',
+                },
+                {
+                    src: 'static/image/logo96.png',
+                    size: '96x96',
+                    type: 'image/png',
+                },
+                {
+                    src: 'static/image/logo144.png',
+                    size: '144x144',
+                    type: 'image/png',
+                },
+                {
+                    src: 'static/image/logo192.png',
+                    size: '192x192',
+                    type: 'image/png',
+                },
+                {
+                    src: 'static/image/logo512.png',
+                    size: '512x512',
+                    type: 'image/png',
+                },
+            ],
+        }),
     ];
 
     if (isAnalyze) {
